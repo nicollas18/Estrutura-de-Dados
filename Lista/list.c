fa1a2ba744deb63e7f62a960a7f4c0bf3c9aa899
NicollasProvatti <74670755+nicollas18@users.noreply.github.com>
2023-08-10 12:53:58 -0300

    Update list.c

--- Lista/list.c
+++ Lista/list.c
@@ -1,27 +1,30 @@
 #include "linked_list.h"
 
+
+
 int main(){
 
     LinkedList *list;
     list = LinkedList_create();
 
-    
+    /*
     LinkedList_add_frist(list, 5);
     LinkedList_add_frist(list, 4);
     LinkedList_add_frist(list, 2);
     LinkedList_add_frist(list, 10);
-    
-
-
-    /*
-    LinkedList_add_last(list,5);
-    LinkedList_add_last(list,4);
-    LinkedList_add_last(list,2);
-    LinkedList_add_last(list,10);
     */
-    
 
     
+    LinkedList_add_last(list,10);
+    LinkedList_add_last(list,2);
+    LinkedList_add_last(list,4);
+    LinkedList_add_last(list,5);
+    LinkedList_add_last(list,7);
+
+    LinkedList_print(list);
+
+    LinkedList_remove(list, 4);
+
     LinkedList_print(list);
     
     return 0;
