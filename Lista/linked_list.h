4007095bf776d1b8f5040ba748b972517d0720a6
NicollasProvatti <74670755+nicollas18@users.noreply.github.com>
2023-08-10 12:54:18 -0300

    Update linked_list.h

--- Lista/linked_list.h
+++ Lista/linked_list.h
@@ -1,6 +1,5 @@
 #ifndef LINKED_LIST
 #define LINKED_LIST
-
 #include <stdbool.h>
 
 typedef struct _simple_node SimpleNode;
@@ -12,6 +11,7 @@ bool LinkedList_is_empty(const LinkedList *L);
 void LinkedList_add_frist(LinkedList *L, int val);
 void LinkedList_add_last(LinkedList *L, int val);
 void LinkedList_add_last_slow(LinkedList *L, int val);
+void LinkedList_remove(LinkedList *L, int val);
 void LinkedList_print(LinkedList *L);
 
 #endif
