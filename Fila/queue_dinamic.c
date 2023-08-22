4d5f9271821f7894b709dc39f5e37e086958ea2c
NicollasProvatti <74670755+nicollas18@users.noreply.github.com>
2023-08-22 14:49:55 -0300

    Update queue_dinamic.c

--- Fila/queue_dinamic.c
+++ Fila/queue_dinamic.c
@@ -1,9 +1,26 @@
 #include "dinamic_queue.h"
+#include <stdio.h>
 
 int main(){
 
-    DinamicQueue *Q = Dicamic_Queue_create();
+    DinamicQueue *Q = DinamicQueue_create();
 
     printf("enqueue:\n");
+    DinamicQueue_enqueue(Q, 10);
+    DinamicQueue_enqueue(Q, 20);
+    DinamicQueue_enqueue(Q, 30);
+    DinamicQueue_enqueue(Q, 40);
+    DinamicQueue_print(Q);
+
+    printf("peek\n");
+    printf("%d\n", DinamicQueue_peek(Q));
+    DinamicQueue_print(Q);
+
+    printf("dequeue\n");
+    DinamicQueue_dequeue(Q);
+    DinamicQueue_dequeue(Q);
+    DinamicQueue_print(Q);
+
+    return 0;
     
 }
