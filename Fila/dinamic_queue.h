7ea6e4b31d06c30e2ffd010af62cda7cd4eeb8a1
NicollasProvatti <74670755+nicollas18@users.noreply.github.com>
2023-08-22 14:49:11 -0300

    Update dinamic_queue.h

--- Fila/dinamic_queue.h
+++ Fila/dinamic_queue.h
@@ -3,18 +3,18 @@
 
 #include <stdbool.h>
 
-typedef struct _static_queue DinamicQueue;
+typedef struct _dinamic_queue DinamicQueue;
 
-DinamicQueue *Dinamic_Queue_create();
+DinamicQueue *DinamicQueue_create();
 void DinamicQueue_destroy(DinamicQueue **q_ref);
 
-bool Dinamic_Queue_is_empty(DinamicQueue *Q);
-long Dinamic_Queue_size(DinamicQueue *Q);
+bool DinamicQueue_is_empty(DinamicQueue *Q);
+long DinamicQueue_size(DinamicQueue *Q);
 
-void Dinamic_Queue_enqueue(DinamicQueue *Q, int val);
-int Dinamic_Queue_peek(DinamicQueue *Q);
-int Dinamic_Queue_dequeue(DinamicQueue *Q);
+void DinamicQueue_enqueue(DinamicQueue *Q, int val);
+int DinamicQueue_peek(DinamicQueue *Q);
+int DinamicQueue_dequeue(DinamicQueue *Q);
 
-void Dinamic_Queue_print(DinamicQueue *Q);
+void DinamicQueue_print(DinamicQueue *Q);
 
 #endif
